fdfca94fb4f7cd6f2e70c04e0419c793864a3a60
NOT-PRO-AMAN <amansingh484748@gmail.com>
2026-02-22 01:29:56 +0530

    given camera permissions to android and changed scanpage to statefull widgets

--- lib/scan_page.dart
+++ lib/scan_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ScanPage extends StatelessWidget {
+class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
 
+  @override
+  State<ScanPage> createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
