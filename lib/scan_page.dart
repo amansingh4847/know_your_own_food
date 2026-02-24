0e4e06f4c813bd35899ef6e0c2bd6edcaff30bfe
NOT-PRO-AMAN <amansingh484748@gmail.com>
2026-02-24 18:28:34 +0530

    added a feature to display tags on screen and adddde a func to pick only main food

--- lib/scan_page.dart
+++ lib/scan_page.dart
@@ -13,59 +13,85 @@ class ScanPage extends StatefulWidget {
 }
 
 class _ScanPageState extends State<ScanPage> {
-
   //image file
   File? image;
 
   //image picker
   final picker = ImagePicker();
 
-
   //pick image method
   Future<void> pickImage(ImageSource source) async {
-
     //pick image from cameraa or gallery
     final PickedFile = await picker.pickImage(source: source);
 
     //update selected image
-    if(PickedFile != null){
+    if (PickedFile != null) {
       setState(() {
         image = File(PickedFile!.path);
       });
     }
   }
 
+  //list to store results
+  List<String> detectedTags = [];
+
   //image converter
   Future<String> convertImageToBase64() async {
-  final bytes = await image!.readAsBytes();
-  return base64Encode(bytes);
+    final bytes = await image!.readAsBytes();
+    return base64Encode(bytes);
   }
 
   Future<void> sendToImagga() async {
-  if (image == null) return;
+    if (image == null) return;
 
-  const apiKey = ApiKeys.imaggaApiKey;
-  const apiSecret = ApiKeys.imaggaApiSecret;
+    const apiKey = ApiKeys.imaggaApiKey;
+    const apiSecret = ApiKeys.imaggaApiSecret;
 
-  var request = http.MultipartRequest(
-    'POST',
-    Uri.parse("https://api.imagga.com/v2/tags"),
-  );
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse("https://api.imagga.com/v2/tags"),
+    );
 
-  request.headers['Authorization'] =
-      'Basic ${base64Encode(utf8.encode('$apiKey:$apiSecret'))}';
+    request.headers['Authorization'] =
+        'Basic ${base64Encode(utf8.encode('$apiKey:$apiSecret'))}';
 
-  request.files.add(
-    await http.MultipartFile.fromPath('image', image!.path),
-  );
+    request.files.add(await http.MultipartFile.fromPath('image', image!.path));
 
-  var response = await request.send();
-  var responseBody = await response.stream.bytesToString();
+    var response = await request.send();
+    var responseBody = await response.stream.bytesToString();
 
-  print("Status: ${response.statusCode}");
-  print("Body: $responseBody");
-}
+    print("Status: ${response.statusCode}");
+    print("Body: $responseBody");
+
+    final data = jsonDecode(responseBody);
+
+    List tags = data["result"]["tags"];
+
+    detectedTags = tags
+        .map<String>((tag) => tag["tag"]["en"].toString())
+        .take(3) // top 5 tags only (optional)
+        .toList();
+
+    setState(() {});
+  }
+
+  //we need to pick main food
   
+  String pickMainFood(List<String> tags) {
+  List<String> ignoreWords = [
+    "food", "meal", "dish", "lunch", "dinner",
+    "snack", "plate", "delicious", "fresh"
+  ];
+
+  for (String tag in tags) {
+    bool isIgnored = ignoreWords.any((word) => tag.contains(word));
+    if (!isIgnored) {
+      return tag; // first valid food
+    }
+  }
+  return tags.isNotEmpty ? tags.first : "food";
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,47 +103,73 @@ class _ScanPageState extends State<ScanPage> {
           SizedBox(
             height: 200,
             width: 200,
-            child: image != null ?
-      
-            //image selected
-            Image.file(image!)
-            :
-            //no image slected
-            const Center(child: Text("NO Image selected"))
+            child: image != null
+                ?
+                  //image selected
+                  Image.file(image!)
+                :
+                  //no image slected
+                  const Center(child: Text("NO Image selected")),
           ),
-      
-      
+
           //buttons
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-      
-              ElevatedButton(onPressed: () => pickImage(ImageSource.camera),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
+              ElevatedButton(
+                onPressed: () => pickImage(ImageSource.camera),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text("camera"),
               ),
-              child: Text("camera")),
-      
-              const SizedBox(width: 16,),
-      
-              ElevatedButton(onPressed: () => pickImage(ImageSource.gallery),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
+
+              const SizedBox(width: 16),
+
+              ElevatedButton(
+                onPressed: () => pickImage(ImageSource.gallery),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text("Gallery"),
               ),
-              child: Text("Gallery")),
-      
             ],
           ),
-          const SizedBox(height: 20,),
-          
-          ElevatedButton(onPressed: image ==  null ? null: sendToImagga,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green,
-            foregroundColor: Colors.white
+          const SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: image == null ? null : sendToImagga,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white,
+            ),
+            child: Text("OK"),
           ),
-          child: Text("OK")),
+
+          const SizedBox(height: 20),
+
+          detectedTags.isEmpty
+              ? const Text("No tags detected yet")
+              : Column(
+                  children: [
+                    const Text(
+                      "Detected Tags",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Wrap(
+                      spacing: 8,
+                      children: detectedTags.map((tag) {
+                        return Chip(label: Text(tag));
+                      }).toList(),
+                    ),
+                  ],
+                ),
         ],
       ),
     );
